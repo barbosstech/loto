edd03738252aee9a93d5566f8507db72e67231eb
barbosstech <69665244+barbosstech@users.noreply.github.com>
2020-11-18 11:25:52 -0300

    Add files via upload

---
+++ loto.py
@@ -0,0 +1,10 @@
+import random
+
+a: int = random.randrange(1,60)
+b: int = random.randrange(1,60)
+c: int = random.randrange(1,60)
+d: int = random.randrange(1,60)
+e: int = random.randrange(1,60)
+f: int = random.randrange(1,60)
+
+print ("Seus números da sorte são: ", a,b,c,d,e,f)
